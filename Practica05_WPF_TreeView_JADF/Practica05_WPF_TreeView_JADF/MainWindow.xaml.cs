4f20918d95103d966d62d214c4b9e3de9f6cb7e7
Jose Doval <dovaldam108@gmail.com>
2020-11-17 21:21:18 +0100

    se arregla el template del textbox para el MouseIsOver en la practica04 WPF

---
+++ Practica05_WPF_TreeView_JADF/Practica05_WPF_TreeView_JADF/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica05_WPF_TreeView_JADF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<String> localidades;
+        public ObservableCollection<Trabajador> trabajadors;
+
+        public static RoutedCommand mostrarTrabajadoresCommand = new RoutedCommand();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            localidades = new ObservableCollection<string>();
+            localidades.Add("FERROL");
+            localidades.Add("BETANZOS");
+            localidades.Add("SADA");
+            localidades.Add("CORUÑA");
+            localidades_ComboBox.ItemsSource = localidades;
+
+
+            trabajadors = new ObservableCollection<Trabajador>();
+            trabajadors.Add(new Trabajador("JOSE", "DOVAL", "BETANZOS", "JEFE", 1));
+            trabajadors.Add(new Trabajador("MANUEL", "ORTEGA", "SADA", "JEFE", 2));
+            trabajadors.Add(new Trabajador("DIEGO", "PEREZ", "FERROL", "JEFE", 0));
+            trabajadors.Add(new Trabajador("JUAN", "SANCHEZ", "BETANZOS", "1º OFICIAL", 0));
+            trabajadors.Add(new Trabajador("MIGUEL", "CASTRO", "SADA", "1º OFICIAL", 0));
+            trabajadors.Add(new Trabajador("JESUS", "IGLESIAS", "BETANZOS", "2º OFICIAL", 0));
+            trabajadors.Add(new Trabajador("PABLO", "SILVA", "CORUÑA", "JEFE", 3));
+            //trabajadores_ListView.ItemsSource = trabajadors;
+
+            CommandBinding mostrarTrabajadoresCommandBinding = new CommandBinding(
+    mostrarTrabajadoresCommand, ExecutemostrarTrabajadoresCommand, CanExecutemostrarTrabajadoresCommand);
+
+        }
+
+        private void ExecutemostrarTrabajadoresCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            var lstTrabjadores = trabajadors.Where(t => t.Localidad == localidades_ComboBox.Text).ToList();
+            trabajadores_ListView.ItemsSource = lstTrabjadores;
+        }
+        private void CanExecutemostrarTrabajadoresCommand(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (localidades_ComboBox.SelectedItem != null) e.CanExecute = true;
+            else e.CanExecute = false;
+        }
+
+        //
+        // Rellenamos el treeView
+        //
+
+        //private void CrearTreeView()
+        //{
+        //    // CREAMOS UNA LISTA PARA GUARDAR LOS ELEMENTOS RAIZ , EN NUESTRO CASO ESTA LISTA SOLO VA TENER UN ELEMENTO RAIZ
+        //    List<Cuadrilla> cuadrillas = new List<Cuadrilla>();
+
+        //    // CREAMOS EL ELEMENTO RAIZ
+        //    Cuadrilla cuadrilla1 = new Cuadrilla() { Nombre = "CUADRILLAS" };
+
+        //    // CREAMOS LAS LOCALIDADES 
+        //    cuadrilla1.Localidades.Add(new Localidad() { Nombre = "FERROL"});
+        //    cuadrilla1.Localidades.Add(new Localidad() { Nombre = "BETANZOS" });
+        //    cuadrilla1.Localidades.Add(new Localidad() { Nombre = "SADA" });
+        //    cuadrilla1.Localidades.Add(new Localidad() { Nombre = "A CORUÑA" });
+
+
+
+
+        //    // BINDEAMOS EL ROOT 
+        //    trabajadores_TreeView.ItemsSource = cuadrillas;
+
+
+
+        //}
+    }
+
+    //public class Cuadrilla
+    //{
+    //    public string Nombre = "CUADRILLAS";
+
+    //    public ObservableCollection<Localidad> Localidades { get; set; }
+    //    public Cuadrilla()
+    //    {
+    //        this.Localidades = new ObservableCollection<Localidad>();
+    //    }
+
+    //}
+
+    //public class Localidad
+    //{
+    //    public string Nombre { get; set; }
+    //    public ObservableCollection<Jefe> jefes { get; set; }
+
+    //    public Localidad()
+    //    {
+    //        jefes = new ObservableCollection<Jefe>();
+    //    }
+    //}
+
+    //public class Jefe
+    //{
+    //    public string Nombre { get; set; }
+    //    public ObservableCollection<Oficial1> primerosOficiales { get; set; }
+
+    //    public Jefe()
+    //    {
+    //        this.primerosOficiales = new ObservableCollection<Oficial1>();
+    //    }
+    //}
+
+    //public class Oficial1
+    //{
+    //    public string Nombre { get; set; }
+    //    public ObservableCollection<Oficial2> segundosOficiales { get; set; }
+
+    //    public Oficial1()
+    //    {
+    //        this.segundosOficiales = new ObservableCollection<Oficial2>();
+    //    }
+    //}
+
+    //public class Oficial2
+    //{
+    //    public string Nombre { get; set; }
+
+    //    public Oficial2() { }
+    //}
+}
---
+++ Practica05_WPF_TreeView_JADF/Practica05_WPF_TreeView_JADF/Trabajador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica05_WPF_TreeView_JADF
+{
+    public class Trabajador
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Localidad { get; set; }
+        public string Cargo { get; set; }
+        public int Id_padre { get; set; }
+
+        public Trabajador(string nombre, string apellido, string localidad, string cargo, int ip)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Localidad = localidad;
+            this.Cargo = cargo;
+            this.Id_padre = ip;
+        }
+
+        public override string ToString()
+        {
+            return Nombre + ", " + Apellido + ", " + Localidad + ", " + Cargo + ", " + Id_padre.ToString();
+        }
+    }
+}
